a3c1a62983f9aa0aeb127028970fbc560d97f5c8
EtashTyagi <51668955+EtashTyagi@users.noreply.github.com>
2022-03-12 09:28:01 +0530

    Update hello_world.asm

--- buffer_overflow/hello_world.asm
+++ buffer_overflow/hello_world.asm
@@ -11,37 +11,37 @@ global _start
 
 section .text
 _start:						; Jump call exec method for shellcode
-	jmp shellcode_caller	; Jump to shellcode_caller
+	jmp	shellcode_caller		; Jump to shellcode_caller
 
 shellcode:
-	pop		rsi				; rsi = Hello world string stack pointer
+	pop	rsi				; rsi = Hello world string stack pointer
 	
-	xor 	rax, rax		; rax = 0
-	add		rax, SYS_WRITE	; rax = SYS_WRITE
+	xor	rax, rax			; rax = 0
+	add	rax, SYS_WRITE			; rax = SYS_WRITE
 
-	xor 	rdi, rdi		; rdi = 0
-	add		rdi, STDOUT		; rdi = STDOUT
+	xor	rdi, rdi			; rdi = 0
+	add	rdi, STDOUT			; rdi = STDOUT
 
-	xor 	rdx, rdx		; rdx = 0
-	add		rdx, MSG_LEN	; rdx = message length
+	xor	rdx, rdx			; rdx = 0
+	add	rdx, MSG_LEN			; rdx = message length
 	syscall					; write hello world to STDOUT
 
-	xor 	rax, rax		; rax = 0
-	add		rax, SYS_EXIT	; rax = SYS_EXIT
+	xor 	rax, rax			; rax = 0
+	add	rax, SYS_EXIT			; rax = SYS_EXIT
 
-	xor		rdi, rdi		; rdi = 0 [EXIT_SUCCESS]
+	xor	rdi, rdi			; rdi = 0 [EXIT_SUCCESS]
 	syscall					; Graceful exit
 
 
-shellcode_caller:		; call the 'shellcode', pass string in stack
-	call shellcode
+shellcode_caller:				; call the 'shellcode', pass string in stack
+	call 	shellcode
 	message: db "Hello World!", 10 ; message
 
 ; Utility data, only constant data must be passed into shellcode
 section .data
  	message_:		db "Hello World!", 10
- 	MSG_LEN:		equ $-message_		; message_length
- 	SYS_WRITE:		equ 1				; write systemcall code
- 	STDOUT:			equ 1				; standart output
-	EXIT_SUCCESS:	equ 0				; exit code success
- 	SYS_EXIT:		equ 60				; exit systemcall code
+ 	MSG_LEN:		equ $-message_	; message_length
+ 	SYS_WRITE:		equ 1		; write systemcall code
+ 	STDOUT:			equ 1		; standart output
+	EXIT_SUCCESS:	equ 0			; exit code success
+ 	SYS_EXIT:		equ 60		; exit systemcall code
